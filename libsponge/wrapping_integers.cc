973477ec827e1841cfee3f0529d67192b015e03a
Ha Tran <hahntrn@stanford.edu>
2020-10-05 13:35:49 -0700

    unwrap passes all tests but still messy

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -10,13 +10,16 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-const uint64_t MOD32 = uint64_t(UINT32_MAX) + 1;
+const uint64_t MOD32 = (1ul << 32);
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    return WrappingInt32{uint32_t((n + isn.raw_value()) % MOD32)};
+    return WrappingInt32{uint32_t((n + isn.raw_value()) % (1ul << 32))};
 }
+
+uint64_t diff(uint64_t a, uint64_t b) { return (a < b) ? b - a : a - b; }
+
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -32,13 +35,21 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     DUMMY_CODE(n, isn, checkpoint);
     //abs_n = n - isn;
     //if (abs_n < 0) abs_n += MOD32;
-    
+    uint64_t n_wraps = checkpoint / (1ul << 32); 
     uint32_t abs_n;
     if (n.raw_value() >= isn.raw_value()) {
         abs_n = n - isn;
     } else {
-        abs_n = MOD32 - (isn - n);
+        abs_n = (1ul << 32) - (isn - n);
     }
+    
+    // a and b one above one below
+    uint64_t a = abs_n + n_wraps * (1ul << 32);
+    n_wraps += a < checkpoint ? 1 : -1;
+    uint64_t b = abs_n + n_wraps * (1ul << 32);
 
-    return abs_n + checkpoint / MOD32;
+    if (diff(checkpoint, a) > diff(checkpoint, b)) return b;
+
+    return a;
 }
+
