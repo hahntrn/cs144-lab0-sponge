01d466d75b98bb5d779ee337e6e0c00a1e734df1
Ha Tran <hahntrn@stanford.edu>
2020-10-05 12:43:32 -0700

    passed wrap, confused about unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -10,12 +10,12 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+const uint64_t MOD32 = uint64_t(UINT32_MAX) + 1;
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{uint32_t((n + isn.raw_value()) % MOD32)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -30,5 +30,15 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    //abs_n = n - isn;
+    //if (abs_n < 0) abs_n += MOD32;
+    
+    uint32_t abs_n;
+    if (n.raw_value() >= isn.raw_value()) {
+        abs_n = n - isn;
+    } else {
+        abs_n = MOD32 - (isn - n);
+    }
+
+    return abs_n + checkpoint / MOD32;
 }
--- libsponge/wrapping_integers.hh
+++ libsponge/wrapping_integers.hh
@@ -3,13 +3,13 @@
 
 #include <cstdint>
 #include <ostream>
+#include <cmath>
 
 //! \brief A 32-bit integer, expressed relative to an arbitrary initial sequence number (ISN)
 //! \note This is used to express TCP sequence numbers (seqno) and acknowledgment numbers (ackno)
 class WrappingInt32 {
   private:
     uint32_t _raw_value;  //!< The raw 32-bit stored integer
-
   public:
     //! Construct from a raw 32-bit unsigned integer
     explicit WrappingInt32(uint32_t raw_value) : _raw_value(raw_value) {}
